5cef9804268cffc58cc785837e392a41a9237905
Rajshree Saraf <>
2021-12-03 15:03:54 -0500

    Removed //AR View

--- ARAnchorPlayground/ContentView.swift
+++ ARAnchorPlayground/ContentView.swift
@@ -67,7 +67,6 @@ struct ContentView : View {
         ZStack {
             
             //App Phase
-            // AR View.
             ARViewContainer(viewModel: viewModel)
             
             if(viewModel.appState == AppState.START){ //AppState.START
